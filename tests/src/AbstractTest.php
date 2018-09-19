5cdcf8fc46e2588f52957bf934266c28488d46d9
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-09-19 13:28:57 +0300

    initial commit

---
+++ tests/src/AbstractTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Nip\Staging\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AbstractTest
+ * @package Nip\Autoloader\Tests
+ */
+abstract class AbstractTest extends TestCase
+{
+    protected $object;
+}
